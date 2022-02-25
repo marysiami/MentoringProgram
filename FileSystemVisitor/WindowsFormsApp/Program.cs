8f35ac0b408706088f48536467a3ecaec89ce9bb
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-02-25 11:52:07 +0100

    Add project files.

---
+++ FileSystemVisitor/WindowsFormsApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogic.FileSystemVisitor;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    internal static class Program
+    {
+        public static IServiceProvider ServiceProvider { get; set; }
+
+        static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IFileSystemService, FileSystemService>();
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            ConfigureServices();
+            Application.Run(new FileSystemForm());
+
+        }
+    }
+}
---
+++ Greeting/ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Your name is...");
+
+var name = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine(BusinessLogic.Greeting.Welcome(name));
+
