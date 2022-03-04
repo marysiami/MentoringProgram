b21b810468392c5cd8ec49fb20b29c48babc4d8f
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-03-04 10:21:47 +0100

    Task2

--- ExceptionHandling/Task1/Program.cs
+++ ExceptionHandling/Task1/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+          
         }
     }
 }
--- ExceptionHandling/Task2/NumberParser.cs
+++ ExceptionHandling/Task2/NumberParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Task2
 {
@@ -6,7 +7,63 @@ namespace Task2
     {
         public int Parse(string stringValue)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(stringValue))
+            {
+                if(stringValue == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+
+            stringValue = stringValue.Trim();
+
+            if (!Regex.IsMatch(stringValue, @"[\d++-]$"))
+            {
+                throw new FormatException();
+            }
+
+            int response = 0;
+            bool minus = false;
+
+            for (int i =0; i < stringValue.Length; i++)
+            {
+                if(i == 0)
+                {
+                    if(stringValue[i] == '-')
+                    {
+                        minus = true;
+                        continue;
+                    }
+                    if (stringValue[i] == '+')
+                    {
+                        continue;
+                    }
+                }                              
+
+                try
+                {
+                    var value = stringValue[i] - '0';
+
+                    if (minus)
+                    {
+                        response = checked(response * 10 - value);
+                    }
+                    else
+                    {
+                        response = checked(response * 10 + value);
+                    }    
+                }
+                catch (OverflowException e)
+                {
+                    throw e;
+                }
+            }    
+
+            return response;
         }
     }
 }
