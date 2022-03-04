be219762dabd1137a9536f987febfc1a6fa1288a
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-03-04 12:04:57 +0100

    Task 1

--- ExceptionHandling/Task1/Program.cs
+++ ExceptionHandling/Task1/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-          
+            while (true)
+            {
+                var value = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine(value[0]);
+                }
+                catch(IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("string is null or empty \nError: "+ ex.Message);
+                    break;
+                }
+                
+            }   
         }
     }
 }
