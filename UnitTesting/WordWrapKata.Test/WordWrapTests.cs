9fa0824d9e579a590dbbf07a53230c31829fba51
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-04-24 19:38:11 +0200

    Creation of projects and initial implementation.
    Creating a WordWrap Kata project and a Word Wrap Kata.Test project, and adding the basic tests and basic implementation of the Wrap method.
    
    Currently added tests do not pass correctly. Changes are added simultaneously to both projects, the first being written in the test project.

---
+++ UnitTesting/WordWrapKata.Test/WordWrapTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace WordWrapKata.Test
+{
+    public class WordWrapTests
+    {
+        [Fact]
+        public void Wrap_GetEmptyString_ReturnsError()
+        {
+            // Arrange
+            var word = string.Empty;
+
+            // Act
+            Action act = () => WordWrap.Wrap(word,1);
+
+            // Assert
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Wrap_GetLeghtShorterThanCol_ReturnsError()
+        {
+            // Arrange
+            var word = "this";
+
+            // Act
+            Action act = () => WordWrap.Wrap(word, 10);
+
+            // Assert
+            act.Should().Throw<Exception>();
+        }
+
+        [Fact]
+        public void Wrap_GetTwoWordsWithSpace_ReturnsWrongString()
+        {
+            // Arrange
+            var word = "word word";
+
+            // Act
+            var result =  WordWrap.Wrap(word, 6);
+
+            // Assert
+            result.Should().NotBe("word\nword");
+        }
+
+        [Fact]
+        public void Wrap_GetThreeWordsWithSpace_ReturnsWrongString()
+        {
+            // Arrange
+            var word = "word word word";
+
+            // Act
+            var result = WordWrap.Wrap(word, 11);
+
+            // Assert
+            result.Should().NotBe("word\nword\nword");
+            result.Should().Be("word word\nword");
+        }
+    }
+}
---
+++ UnitTesting/WordWrapKata/WordWrap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WordWrapKata
+{
+    public static class WordWrap
+    {
+        public static string Wrap(string term, int length)
+        {
+            if (string.IsNullOrEmpty(term))
+            {
+                throw new ArgumentException(term);
+            }
+
+            if (term.Length <= length)
+            {
+                return term;                  
+            }
+            else
+            {
+                int lastSpace = 0;
+                int space;
+                while ((space = term.IndexOf(" ", lastSpace)) != -1)
+                {
+                    if (space > length)
+                    {
+                        term = term.Substring(0, lastSpace) + "\n" + term.Substring(lastSpace + 1);                       
+                    }
+                    lastSpace = space;
+                }
+            }
+
+            return term;
+
+            //return word.Replace(" ", "\n");
+        }
+    }
+}
