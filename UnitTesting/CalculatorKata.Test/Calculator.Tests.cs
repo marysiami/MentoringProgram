5d0918e6d7cbc6f8940b241dda035438f512cec7
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-04-25 17:29:03 +0200

    Creating the Calculator and Kalkulator.Test projects.
    I created a basic version of the Add method in the Calculator class. Then I added unit tests with the basic definition of the input and output parameters in the Add method. Next, I adjusted the Add method to meet my testing requirements.

---
+++ UnitTesting/CalculatorKata.Test/Calculator.Tests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculatorKata.Test
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_GetEmptyString_ReturnZero()
+        {
+            // Arrange
+            var word = string.Empty;
+
+            // Act
+            var result = Calculator.Add(word);
+
+            // Assert
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Add_GetStringWithOneValue_ReturnStringNumber()
+        {
+            // Arrange
+            var word = "1";
+
+            // Act
+            var result = Calculator.Add(word);
+
+            // Assert
+            result.Should().Be(1);
+        }
+
+
+        [Fact]
+        public void Add_GetStringWithTwoValues_ReturnSum()
+        {
+            // Arrange
+            var word = "1,2";
+
+            // Act
+            var result = Calculator.Add(word);
+
+            // Assert
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Add_GetWrongString_ReturnException()
+        {
+            // Arrange
+            var word = "aaa,77";
+
+            // Act
+            var result = Calculator.Add(word);
+
+            // Assert
+            result.Should().Be(0);
+        }
+    }
+}
---
+++ UnitTesting/CalculatorKata/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorKata
+{
+    public static class Calculator
+    {
+        public static int Add(string numbers)
+        {
+            if(string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            var list = numbers.Split(',');
+
+            var intList = new List<int>();
+
+            foreach(var item in list)
+            {
+                if (Int32.TryParse(item, out int i))
+                {
+                    intList.Add(i);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return intList.Sum();            
+        }
+    }
+}
