c6f7fda4171c7128e477b5110d21da77464999e7
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-05-12 21:13:39 +0200

    Task 2 -init

---
+++ Serialization/CustomSerialization/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomSerialization
+{
+    public class Department
+    {
+        public string DepartmentName { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        public Department(string name, List<Employee> list)
+        {
+            DepartmentName = name;
+            Employees = list;
+        }
+    }
+}
---
+++ Serialization/CustomSerialization/Program.cs
@@ -0,0 +1,36 @@
+﻿using CustomSerialization;
+
+
+var data = InitData();
+Serialize(data);
+Deserialize();
+
+void Serialize(List<Department> departments)
+{
+
+}
+
+void Deserialize()
+{
+
+   
+}
+
+List<Department> InitData()
+{
+    var employees = new List<Employee>
+    {
+        new Employee("Anna"),
+        new Employee("Rob"),
+        new Employee("Steen")
+    };
+
+    var departments = new List<Department>
+    {
+        new Department("DepName", employees),
+        new Department("DepName2", employees),
+        new Department("DepName3", employees)
+    };
+
+    return departments;
+}
